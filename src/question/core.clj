a54bf1a3ea399b1f0d0b8bb3c74c7a4d7453c219
William McPherson <willmcpherson2@gmail.com>
2023-11-15 21:33:22 +1100

    Optimise macro code
    - Doesn't call other macros
    - More readable generated code
    - Maybe faster code generation
    - Maybe faster code
    - Fixes misuse of if-let

--- src/question/core.clj
+++ src/question/core.clj
@@ -10,27 +10,37 @@
 (declare ?)
 
 (defmacro ?seq [args pats body]
-  (if-let [[pat & pats] pats]
-    (if (= pat '&)
-      (if-let [[pat & pats] pats]
-        (if (= pats nil)
-          `(? ~args ~pat ~body)
-          (throw (IllegalArgumentException. "too many arguments after &")))
-        (throw (IllegalArgumentException. "missing argument after &")))
-      (let [a (gensym)
-            as (gensym)]
-        `(if-let [[~a & ~as] ~args]
-           (? ~a ~pat (?seq ~as ~pats ~body)))))
+  (if pats
+    (let* [pat (first pats)]
+          (if (= pat '&)
+            (let* [pats (next pats)]
+                  (if pats
+                    (let* [pat (first pats)]
+                          (if (next pats)
+                            (throw (IllegalArgumentException. "too many arguments after &"))
+                            `(? ~args ~pat ~body)))
+                    (throw (IllegalArgumentException. "missing argument after &"))))
+            (let* [a (gensym)
+                   as (gensym)]
+                  `(let* [~a (first ~args)
+                          ~as (next ~args)]
+                         (? ~a ~pat (?seq ~as ~(next pats) ~body))))))
     body))
 
 (defmacro ? [arg pat body]
-  (cond
-    (= pat '_) body
-    (symbol? pat) `(let [~pat ~arg] ~body)
-    (seqable? pat) `(if (= (type ~arg) ~(type pat))
-                      (?seq ~arg ~pat ~body))
-    :else `(if (= ~arg ~pat)
-             ~body)))
+  (if (symbol? pat)
+    (if (= pat '_)
+      body
+      `(let* [~pat ~arg] ~body))
+    (if (seqable? pat)
+      (let* [a (gensym)
+             as (gensym)]
+            `(let* [~a ~arg]
+                   (if (= (type ~a) ~(type pat))
+                     (let* [~as (seq ~a)]
+                           (?seq ~as ~pat ~body)))))
+      `(if (= ~arg ~pat)
+         ~body))))
 
 (defn main []
   (let [e '(? [1 2 3]
